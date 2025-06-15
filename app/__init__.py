f127beab06ffce8553b826ee8badd34b253e6aed
Divyanshu dwivedi <divyanshpandiit@gmail.com>
2025-06-15 14:22:11 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from config import Config
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -11,7 +12,11 @@ bcrypt = Bcrypt()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
+    # Railway PostgreSQL configuration
+    if 'DATABASE_URL' in os.environ:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
+    
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
