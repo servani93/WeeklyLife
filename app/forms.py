68151f4589b28af8a4d26eeab2fb850cee7abcb2
Divyanshu dwivedi <divyanshpandiit@gmail.com>
2025-06-15 15:38:18 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
+from flask_wtf.file import FileAllowed
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -20,3 +21,10 @@ class EventForm(FlaskForm):
     category = StringField('Category', validators=[DataRequired()])
     type = SelectField('Type', choices=[('personal', 'Personal'), ('global', 'Global')], validators=[DataRequired()])
     submit = SubmitField('Save Event')
+
+class ProfileUpdateForm(FlaskForm):   # 👈 You missed this most likely
+    name = StringField('Name', validators=[Optional(), Length(min=2, max=100)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    birthdate = DateField('Birthdate', validators=[DataRequired()])
+    profile_pic = FileField('Update Profile Picture', validators=[Optional(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    submit = SubmitField('Update Profile')
